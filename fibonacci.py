5406d1c692bc1e1c694a7e71dd751fbc52775cc1
vanesavega <101160634+vanesavega@users.noreply.github.com>
2024-11-29 21:40:51 -0500

    Corrección errores

--- fibonacci.py
+++ fibonacci.py
@@ -9,13 +9,13 @@ def generar_fibonacci(cantidad):
     RETORNA:
         lista: Una lista con los números de la secuencia de Fibonacci.
     """
-    " ----------------- validación -------------------------------------
+    # ----------------- validación -------------------------------------
     if cantidad <= 0:  #validamos que  el parámetro Cantidad sea positivo y mayor que 0
         return []  
     if cantidad == 1:
         return [0]  # Si solo se necesita un número, retorna [0]
 
-    " ----------------- función -------------------------------------
+    # ----------------- función -------------------------------------
     fibonacci = [0, 1]  # Los dos primeros números de la secuencia
     for _ in range(2, cantidad):
         fibonacci.append(fibonacci[-1] + fibonacci[-2]) #calcula la serie de fibonacci
