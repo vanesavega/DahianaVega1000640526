67828a0658763bee69645f9f242221d6f7357ac0
vanesavega <101160634+vanesavega@users.noreply.github.com>
2024-11-29 21:17:43 -0500

    Explicar funcion Fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -2,15 +2,23 @@ def saludo():
     print("¡Hola! este codigo muestra los primeros 10 números de la secuencia de Fibonacci :")
 
 def generar_fibonacci(cantidad):
-    fibonacci = [0, 1]
+    """
+    Genera una lista con los primeros números de la serie de Fibonacci usando como parámtero Cantidad.
+    PARAMETROS:
+        cantidad (int): La cantidad de números a generar.
+    RETORNA:
+        lista: Una lista con los números de la secuencia de Fibonacci.
+    """
+    
+    fibonacci = [0, 1]  # Los dos primeros números de la secuencia
     for _ in range(2, cantidad):
-        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+        fibonacci.append(fibonacci[-1] + fibonacci[-2]) #calcula la serie de fibonacci
     return fibonacci
 
 def main():
     saludo()
-    numeros_fibonacci = generar_fibonacci(10)
-    print(numeros_fibonacci)
+    numeros_fibonacci = generar_fibonacci(20) #se ejecuta la funcion usando como parámetro el número 20
+    print(numeros_fibonacci) # da la serie de fibonacci hasta el número 20
 
 if __name__ == "__main__":
     main()
